e21328b60c784377408241868d855243ccdf62c6
SM7 <smsajid7300@gmail.com>
2021-02-07 18:22:46 +0600

    main assignment done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function searchingItem(params) {
+function searchingItem() {
+    document.getElementById('food-container').innerHTML = ``
     const inputValue = document.getElementById('item-searched').value;
     const inputLength = inputValue.length;
     if (inputLength == 1) {
@@ -20,25 +21,29 @@ function foodName(mealName) {
 }
 
 const displayFoodItem = mealItem => {
-    console.log(mealItem);
-    const mealDiv = document.getElementById('food-container');
-    const arrayItem = mealItem.meals
-    arrayItem.forEach(meal => {
+    const items = mealItem.meals;
+    items.forEach(meal => {
+        //previous cod
+        const mealDiv = document.getElementById('food-container');
+        console.log("here: ", meal);
         const mealContainer = document.getElementById('food-items');
         const mealName = meal.strMeal
         const mealId = meal.idMeal
         const mealThumb = meal.strMealThumb
         const mealDetails = `
-    <div class="col">
-                <div class="card">
-                    <img src="${mealThumb}" class="card-img-top " alt="${mealId}">
-                    <div class="card-body">
-                        <h5 class="card-title">${mealName}</h5>
-                    </div>
+        <div class="col">
+        <div class="card card-style">
+            <a href="#${mealId}">
+                <img src="${mealThumb}" class="card-img-top " alt="${mealId}">
+                <div class="card-body">
+                    <h5 class="card-title">${mealName}</h5>
                 </div>
+            </a>
             </div>
-    `
-        mealDiv.innerHTML = mealDetails
+        </div>
+        `
+        mealDiv.innerHTML += mealDetails
         mealContainer.appendChild(mealDiv)
-    })
+        //previous cod
+    });
 }
